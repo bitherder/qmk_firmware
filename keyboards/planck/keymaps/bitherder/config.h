47268e6a62080a0cd5f86891c7e2ad59a395fd9d
Larry Baltz <larry@baltz.org>
2019-07-25 17:59:13 +0200

    Reconfigure mousekey for Planck

--- keyboards/planck/keymaps/bitherder/config.h
+++ keyboards/planck/keymaps/bitherder/config.h
@@ -9,4 +9,11 @@
 
 #define MUSIC_MASK (keycode != KC_NO)
 
+// Mousekey configuration
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_DELAY 0
+
 #endif
