091be5f1a78bd20b09bc6e0085c320a84c98137f
Larry Baltz <larry@baltz.org>
2026-01-17 11:13:18 +0100

    iris:bitherder:taipo: all the keys are working

--- keyboards/keebio/iris/keymaps/bitherder/keymap.c
+++ keyboards/keebio/iris/keymaps/bitherder/keymap.c
@@ -34,6 +34,10 @@ enum custom_keycodes {
   DQTD,
   QTGV,
   DTEX,
+  MY_LGUI,
+  MY_LALT,
+  MY_LCTL,
+  MY_LSFT,
   QWERTY,
   ARENSITO,
   TAIPO,
@@ -168,6 +172,219 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   )
+};
+
+
+const uint16_t PROGMEM cm_s_e[] = {KC_E, KC_SPC, COMBO_END};
+const uint16_t PROGMEM cm_s_t[] = {KC_T,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_o[] = {KC_O,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_a[] = {KC_A,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_lprn[] = {KC_E,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_lbrc[] = {KC_T,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_lcbr[] = {KC_O,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_lt[] = {KC_A,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_s_i[] = {KC_I,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_n[] = {KC_N,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_s[] = {KC_S,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_r[] = {KC_R,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_rprn[] = {KC_I,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_rbrc[] = {KC_N,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_rcbr[] = {KC_S,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_gt[] = {KC_R,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_h[] = {KC_E,KC_T,COMBO_END};
+const uint16_t PROGMEM cm_l[] = {KC_O,KC_A,COMBO_END};
+const uint16_t PROGMEM cm_y[] = {KC_I,KC_N,COMBO_END};
+const uint16_t PROGMEM cm_b[] = {KC_S,KC_R,COMBO_END};
+const uint16_t PROGMEM cm_s_h[] = {KC_E,KC_T,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_l[] = {KC_O,KC_A,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_y[] = {KC_I,KC_N,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_b[] = {KC_S,KC_R,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_0[] = {KC_E,KC_T,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_4[] = {KC_O,KC_A,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_5[] = {KC_I,KC_N,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_9[] = {KC_S,KC_R,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_g[] = {KC_I,KC_R,COMBO_END};
+const uint16_t PROGMEM cm_u[] = {KC_T,KC_O,COMBO_END};
+const uint16_t PROGMEM cm_d[] = {KC_E,KC_A,COMBO_END};
+const uint16_t PROGMEM cm_p[] = {KC_N,KC_S,COMBO_END};
+const uint16_t PROGMEM cm_s_g[] = {KC_I,KC_R,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_u[] = {KC_T,KC_O,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_d[] = {KC_E,KC_A,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_p[] = {KC_N,KC_S,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_hash[] = {KC_I,KC_R,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_2[] = {KC_T,KC_O,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_at[] = {KC_E,KC_A,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_7[] = {KC_N,KC_S,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_c[] = {KC_E,KC_O,COMBO_END};
+const uint16_t PROGMEM cm_q[] = {KC_T,KC_A,COMBO_END};
+const uint16_t PROGMEM cm_f[] = {KC_I,KC_S,COMBO_END};
+const uint16_t PROGMEM cm_z[] = {KC_N,KC_R,COMBO_END};
+const uint16_t PROGMEM cm_s_c[] = {KC_E,KC_O,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_q[] = {KC_T,KC_A,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_f[] = {KC_I,KC_S,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_z[] = {KC_N,KC_R,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_1[] = {KC_E,KC_O,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_3[] = {KC_T,KC_A,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_6[] = {KC_I,KC_S,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_8[] = {KC_N,KC_R,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_v[] = {KC_S,KC_E,COMBO_END};
+const uint16_t PROGMEM cm_x[] = {KC_R,KC_T,COMBO_END};
+const uint16_t PROGMEM cm_k[] = {KC_I,KC_O,COMBO_END};
+const uint16_t PROGMEM cm_j[] = {KC_N,KC_A,COMBO_END};
+const uint16_t PROGMEM cm_s_v[] = {KC_S,KC_E,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_x[] = {KC_R,KC_T,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_k[] = {KC_I,KC_O,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_j[] = {KC_N,KC_A,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_astr[] = {KC_S,KC_E,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_circ[] = {KC_R,KC_T,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_plus[] = {KC_I,KC_O,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_eql[] = {KC_N,KC_A,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_mins[] = {KC_N,KC_O,COMBO_END};
+const uint16_t PROGMEM cm_w[] = {KC_I,KC_A,COMBO_END};
+const uint16_t PROGMEM cm_m[] = {KC_R,KC_E,COMBO_END};
+const uint16_t PROGMEM cm_slsh[] = {KC_S,KC_T,COMBO_END};
+const uint16_t PROGMEM cm_unds[] = {KC_N,KC_O,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_w[] = {KC_I,KC_A,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_s_m[] = {KC_R,KC_E,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_bsls[] = {KC_S,KC_T,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_perc[] = {KC_N,KC_O,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_ampr[] = {KC_I,KC_A,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_dlr[] = {KC_R,KC_E,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_pipe[] = {KC_S,KC_T,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_quot[] = {KC_S,KC_A,COMBO_END};
+const uint16_t PROGMEM cm_scln[] = {KC_R,KC_O,COMBO_END};
+const uint16_t PROGMEM cm_ques[] = {KC_I,KC_T,COMBO_END};
+const uint16_t PROGMEM cm_comma[] = {KC_N,KC_E,COMBO_END};
+const uint16_t PROGMEM cm_dqt[] = {KC_S,KC_A,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_coln[] = {KC_R,KC_O,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_exlm[] = {KC_I,KC_T,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_dot[] = {KC_N,KC_E,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_grave[] = {KC_S,KC_A,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_tilde[] = {KC_N,KC_E,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_enter[] = {KC_E,KC_T,KC_O,COMBO_END};
+const uint16_t PROGMEM cm_tab[] = {KC_I,KC_N,KC_S,COMBO_END};
+const uint16_t PROGMEM cm_esc[] = {KC_E,KC_T,KC_O,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_del[] = {KC_I,KC_N,KC_S,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_algr[] = {KC_E,KC_T,KC_O,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_lead[] = {KC_I,KC_N,KC_S,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_lgui[] = {KC_A,KC_R,COMBO_END};
+const uint16_t PROGMEM cm_lalt[] = {KC_O,KC_S,COMBO_END};
+const uint16_t PROGMEM cm_lctl[] = {KC_T,KC_N,COMBO_END};
+const uint16_t PROGMEM cm_lsft[] = {KC_E,KC_I,COMBO_END};
+const uint16_t PROGMEM cm_right[] = {KC_A,KC_R,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_up[] = {KC_O,KC_S,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_down[] = {KC_T,KC_N,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_left[] = {KC_E,KC_I,KC_SPC,COMBO_END};
+const uint16_t PROGMEM cm_pgup[] = {KC_A,KC_R,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_home[] = {KC_O,KC_S,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_end[] = {KC_T,KC_N,KC_BSPC,COMBO_END};
+const uint16_t PROGMEM cm_pgdn[] = {KC_E,KC_I,KC_BSPC,COMBO_END};
+
+combo_t key_combos[] = {
+  COMBO(cm_s_e,S(KC_E)),
+  COMBO(cm_s_t,S(KC_T)),
+  COMBO(cm_s_o,S(KC_O)),
+  COMBO(cm_s_a,S(KC_A)),
+  COMBO(cm_lprn,KC_LPRN),
+  COMBO(cm_lbrc,KC_LBRC),
+  COMBO(cm_lcbr,KC_LCBR),
+  COMBO(cm_lt,KC_LT),
+  COMBO(cm_s_i,S(KC_I)),
+  COMBO(cm_s_n,S(KC_N)),
+  COMBO(cm_s_s,S(KC_S)),
+  COMBO(cm_s_r,S(KC_R)),
+  COMBO(cm_rprn,KC_RPRN),
+  COMBO(cm_rbrc,KC_RBRC),
+  COMBO(cm_rcbr,KC_RCBR),
+  COMBO(cm_gt,KC_GT),
+  COMBO(cm_h,KC_H),
+  COMBO(cm_l,KC_L),
+  COMBO(cm_y,KC_Y),
+  COMBO(cm_b,KC_B),
+  COMBO(cm_s_h,S(KC_H)),
+  COMBO(cm_s_l,S(KC_L)),
+  COMBO(cm_s_y,S(KC_Y)),
+  COMBO(cm_s_b,S(KC_B)),
+  COMBO(cm_0,KC_0),
+  COMBO(cm_4,KC_4),
+  COMBO(cm_5,KC_5),
+  COMBO(cm_9,KC_9),
+  COMBO(cm_g,KC_G),
+  COMBO(cm_u,KC_U),
+  COMBO(cm_d,KC_D),
+  COMBO(cm_p,KC_P),
+  COMBO(cm_s_g,S(KC_G)),
+  COMBO(cm_s_u,S(KC_U)),
+  COMBO(cm_s_d,S(KC_D)),
+  COMBO(cm_s_p,S(KC_P)),
+  COMBO(cm_hash,KC_HASH),
+  COMBO(cm_2,KC_2),
+  COMBO(cm_at,KC_AT),
+  COMBO(cm_7,KC_7),
+  COMBO(cm_c,KC_C),
+  COMBO(cm_q,KC_Q),
+  COMBO(cm_f,KC_F),
+  COMBO(cm_z,KC_Z),
+  COMBO(cm_s_c,S(KC_C)),
+  COMBO(cm_s_q,S(KC_Q)),
+  COMBO(cm_s_f,S(KC_F)),
+  COMBO(cm_s_z,S(KC_Z)),
+  COMBO(cm_1,KC_1),
+  COMBO(cm_3,KC_3),
+  COMBO(cm_6,KC_6),
+  COMBO(cm_8,KC_8),
+  COMBO(cm_v,KC_V),
+  COMBO(cm_x,KC_X),
+  COMBO(cm_k,KC_K),
+  COMBO(cm_j,KC_J),
+  COMBO(cm_s_v,S(KC_V)),
+  COMBO(cm_s_x,S(KC_X)),
+  COMBO(cm_s_k,S(KC_K)),
+  COMBO(cm_s_j,S(KC_J)),
+  COMBO(cm_astr,KC_ASTR),
+  COMBO(cm_circ,KC_CIRC),
+  COMBO(cm_plus,KC_PLUS),
+  COMBO(cm_eql,KC_EQL),
+  COMBO(cm_mins,KC_MINS),
+  COMBO(cm_w,KC_W),
+  COMBO(cm_m,KC_M),
+  COMBO(cm_slsh,KC_SLSH),
+  COMBO(cm_unds,KC_UNDS),
+  COMBO(cm_s_w,S(KC_W)),
+  COMBO(cm_s_m,S(KC_M)),
+  COMBO(cm_bsls,KC_BSLS),
+  COMBO(cm_perc,KC_PERC),
+  COMBO(cm_ampr,KC_AMPR),
+  COMBO(cm_dlr,KC_DLR),
+  COMBO(cm_pipe,KC_PIPE),
+  COMBO(cm_quot,KC_QUOT),
+  COMBO(cm_scln,KC_SCLN),
+  COMBO(cm_ques,KC_QUES),
+  COMBO(cm_comma,KC_COMMA),
+  COMBO(cm_dqt,KC_DQT),
+  COMBO(cm_coln,KC_COLN),
+  COMBO(cm_exlm,KC_EXLM),
+  COMBO(cm_dot,KC_DOT),
+  COMBO(cm_grave,KC_GRAVE),
+  COMBO(cm_tilde,KC_TILDE),
+  COMBO(cm_enter,KC_ENTER),
+  COMBO(cm_tab,KC_TAB),
+  COMBO(cm_esc,KC_ESC),
+  COMBO(cm_del,KC_DEL),
+  COMBO(cm_algr,KC_ALGR),
+  COMBO(cm_lead,QK_LEAD),
+  COMBO(cm_lgui,MY_LGUI),
+  COMBO(cm_lalt,MY_LALT),
+  COMBO(cm_lctl,MY_LCTL),
+  COMBO(cm_lsft,MY_LSFT),
+  COMBO(cm_right,KC_RIGHT),
+  COMBO(cm_up,KC_UP),
+  COMBO(cm_down,KC_DOWN),
+  COMBO(cm_left,KC_LEFT),
+  COMBO(cm_pgup,KC_PGUP),
+  COMBO(cm_home,KC_HOME),
+  COMBO(cm_end,KC_END),
+  COMBO(cm_pgdn,KC_PGDN),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
